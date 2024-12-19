dfa52adef80f429968eb480a894bedb4711ad03c
alperenbekci <alperenbekci@hotmail.com>
2024-12-19 21:17:47 +0300

    fix: Update footer open source link

--- next-app/src/app/components/Footer.jsx
+++ next-app/src/app/components/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="flex  justify-between align-items gap-2  bg-slate-50  p-4">
       <p className="font-bold  text-blue-600">
-        <Link href="https://github.com/alperenbekci">Open Source</Link>
+        <Link href="https://github.com/alperenbekci/forever-messages">
+          Open Source
+        </Link>
       </p>
       <p className="text-black">Builded in JSM Hackathon💙</p>
     </footer>
