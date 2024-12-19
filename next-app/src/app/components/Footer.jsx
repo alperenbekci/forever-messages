93c3c44543ebfede5d49619a7afbe5db9bb636ec
alperenbekci <alperenbekci@hotmail.com>
2024-12-19 21:07:04 +0300

    chore: core project initialized

---
+++ next-app/src/app/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="flex  justify-between align-items gap-2  bg-slate-50  p-4">
+      <p className="font-bold  text-blue-600">
+        <Link href="https://github.com/alperenbekci">Open Source</Link>
+      </p>
+      <p className="text-black">Builded in JSM Hackathon💙</p>
+    </footer>
+  );
+};
+
+export default Footer;
